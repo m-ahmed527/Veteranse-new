86ad995d4ab729460bd09db81e4b26b586a3dca4
m-ahmed527 <faizanraza527@gmail.com>
2025-07-18 03:52:14 +0500

    18-7-25

--- app/Http/Controllers/Api/Vendor/OrderController.php
+++ app/Http/Controllers/Api/Vendor/OrderController.php
@@ -8,9 +8,7 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
         try {
